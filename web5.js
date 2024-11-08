84c00900bb3365bb2d819ac877eb5328ebeb4b50
Vasylyna-Halukh <vasylyna.halukh@lnu.edu.ua>
2024-11-08 19:35:07 +0200

    Запустила сервер

--- web5.js
+++ web5.js
@@ -1,1 +1,24 @@
-ECHO is on.
+// web.js
+const express = require('express');
+const commander = require('commander');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Commander.js example (basic command)
+commander
+  .command('sayHello')
+  .description('Say hello')
+  .action(() => {
+    console.log('Hello from Commander.js!');
+  });
+
+commander.parse(process.argv);
